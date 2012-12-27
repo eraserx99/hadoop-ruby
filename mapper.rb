22c08c00788e69d638fd69243542148b890e2aa4
Steve Chien <steve@productlab.com>
2012-12-27 12:31:09 -0500

    changes

--- mapper.rb
+++ mapper.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/ruby
-
+count = 0
 ARGF.each do |line|
   line = line.chomp
   
-  (key, value) = line.split(/\t/)
+  # (key, value) = line.split(/\t/)
   
-  puts key + "\t" + value if key != nil and value != nil
-  puts key if key != nil and value == nil 
+  # puts key + "\t" + value if key != nil and value != nil
+  # puts key if key != nil and value == nil 
+  # puts count.to_s + "\t" + key if key != nil and value == nil 
+  puts count.to_s + "\t" + line if line != nil
+  count += 1
 end
 
